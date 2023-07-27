af8d75436dff491ef92539e0bc8519b91c0ac062
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-07-27 14:52:36 +0800

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,3 +1,10 @@
 import streanlit
 
 streamlit.title('MY Parent New Healthy Diner') 
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale Spinach & Rocket Smoothie')
+streamlit.text('Hard Boil Free Rage Egg')
+
