4ab7d594db3c63071b117dd745942272f98d02e8
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-07-27 15:02:26 +0800

    Update first_streamlit_app.py
    added Breakfast menu and 3 Items

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -3,8 +3,11 @@ import streamlit
 streamlit.title('MY Parent New Healthy Diner') 
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale Spinach & Rocket Smoothie')
-streamlit.text('Hard Boil Free Rage Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard Boil Free Rage Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
