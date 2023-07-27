c2c8c514e149acfa56236cd486c430d5d9e4f143
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-07-27 15:30:00 +0800

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -19,3 +19,5 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
