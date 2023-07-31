e2cab64c5d1270ff4edba6ca7d2f18371214bfed
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-07-31 14:21:08 +0800

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -45,3 +45,14 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
+
+add_my_fruit = streamlit.text_input('What would you like to add?','jackfruit')
+streamlit.write('Thanks for adding', add_my_fruit)
+
+
