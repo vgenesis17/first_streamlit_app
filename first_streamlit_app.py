2362517a2e188df74197fb8d8bf311232944231f
vgenesis17 <140689260+vgenesis17@users.noreply.github.com>
2023-07-31 14:26:23 +0800

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -46,9 +46,6 @@ streamlit.text("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
 
 
